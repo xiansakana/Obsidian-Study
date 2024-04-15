611eb1ead7680c99c3d2ead12d7c6dac2172c553
xiansakana <2461298052@qq.com>
2024-04-15 19:31:47 -0400

    vault backup: 2024-04-15 19:31:47

--- replace_md_url_backblaze2jsdelivr.py
+++ replace_md_url_backblaze2jsdelivr.py
@@ -19,9 +19,9 @@ def replace_image_links_in_directory(directory, old_link_prefix, new_link_prefix
                     file.write(new_content)
 
 # 指定目录和需要替换的链接前缀
-directory_path = 'd:/notes/study/itheima/microservice'
+directory_path = 'd:/notes/study/itheima/redis'
 old_link_prefix = 'img.xiansakana.xyz'
-# old_link_prefix = 'assets/'
+# old_link_prefix = ".assets/"
 # old_link_prefix = 'zui-xin-ban-java-web-kai-fa-jiao-cheng1.0.2/assets2/'
 # old_link_prefix = 'https://cdn.jsdelivr.net/npm/itheima-ssm/assets/'
 
@@ -29,8 +29,8 @@ old_link_prefix = 'img.xiansakana.xyz'
 # old_link_prefix = 'cdn.jsdelivr.net/npm/xiansakana-blog-img'
 
 # new_link_prefix = 'img.xiansakana.xyz'
-# new_link_prefix = 'cdn.cbd.int/xiansakana-blog-img'
-new_link_prefix = 'cdn.jsdelivr.net/npm/xiansakana-blog-cover'
+new_link_prefix = 'cdn.cbd.int/xiansakana-blog-img'
+# new_link_prefix = 'https://cdn.jsdelivr.net/npm/redis-itheima/'
 # new_link_prefix = 'https://cdn.jsdelivr.net/npm/microservice-springcloud-rabbitmq-docker-redis-es/'
 # new_link_prefix = 'zui-xin-ban-java-web-kai-fa-jiao-cheng@1.0.2/assets2/'
 
